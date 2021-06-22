92b3eca5f4db9930ddef949e3516e99a35988826
Pranitha Reddy Kona <pranithakona@fb.com>
2021-06-22 14:11:11 -0500

    Initial Commit

---
+++ Tip Calculator/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Tip Calculator
+//
+//  Created by Pranitha Reddy Kona on 6/22/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tip Calculator/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Tip Calculator
+//
+//  Created by Pranitha Reddy Kona on 6/22/21.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
