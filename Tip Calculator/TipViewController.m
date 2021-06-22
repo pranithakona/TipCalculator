7285b30c17a838bd384dd7666c281177c056b4d9
Pranitha Reddy Kona <pranithakona@fb.com>
2021-06-22 16:05:36 -0500

    basic capabilitiescomplete

--- Tip Calculator/TipViewController.m
+++ Tip Calculator/TipViewController.m
@@ -8,6 +8,10 @@
 #import "TipViewController.h"
 
 @interface TipViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipPercentControl;
 
 @end
 
@@ -17,6 +21,20 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:true];
+}
+- (IBAction)updateLabels:(id)sender {
+    double tipPercents[] = {0.15, 0.2, 0.25};
+    double tipPercent = tipPercents[self.tipPercentControl.selectedSegmentIndex];
+    double tip = [self.billField.text doubleValue] * tipPercent;
+    double total = [self.billField.text doubleValue] + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+}
+
+\
 
 /*
 #pragma mark - Navigation
